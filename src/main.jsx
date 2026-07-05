8d48783fa7c8929626818f94564efb0308fb25cb
jooguda-protomot <jooguda@gmail.com>
2026-07-05 23:03:34 +0200

    Oprava main.jsx - admin routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import AdminPanel from "./AdminPanel.jsx";
 
-// Ak URL obsahuje /admin, zobraz AdminPanel
 const isAdmin = window.location.pathname === "/admin";
-ReactDOM.createRoot(document.getElementById("root")).render(
-  isAdmin ? <AdminPanel /> : <App />
-);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {isAdmin ? <AdminPanel /> : <App />}
+  </StrictMode>
+);
